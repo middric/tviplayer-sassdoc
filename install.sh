21eb396e6d3032208208b8ac9a5e4bd25bfb3506
middric <richmiddleditch@gmail.com>
2013-05-26 11:00:15 +0100

    adding some colour output

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-echo "\033[36m Setting up TV & iPlayer Sassdoc\033[0m"
-echo "\033[36m ===============================\033[0m"
+echo -e "\033[36mSetting up TV & iPlayer Sassdoc\033[0m"
+echo -e "\033[36m===============================\033[0m"
 yum install git nodejs python26 ruby rubygems -y
 gem install compass -y
 npm -g install forever
 
-echo "\033[36m Cloning tviplayer-sassdoc from github\033[0m"
-echo "\033[36m =====================================\033[0m"
+echo -e "\033[36mCloning tviplayer-sassdoc from github\033[0m"
+echo -e "\033[36m=====================================\033[0m"
 git clone --recursive https://github.com/middric/tviplayer-sassdoc.git
 cd tviplayer-sassdoc
 
-echo "\033[36m Setting up sassdoc\033[0m"
-echo "\033[36m ==================\033[0m"
+echo -e "\033[36mSetting up sassdoc\033[0m"
+echo -e "\033[36m==================\033[0m"
 cd sassdoc
 npm install --python=python26
 
-echo "\033[36m Seting up sassdoc as a service\033[0m"
-echo "\033[36m ==============================\033[0m"
+echo -e "\033[36mSeting up sassdoc as a service\033[0m"
+echo -e "\033[36m==============================\033[0m"
 cd ..
 cp sassdoc-service /etc/init.d/sassdoc
 chmod a+x /etc/init.d/sassdoc
 
-echo "\033[36m Setting sassdoc to run on startup\033[0m"
-echo "\033[36m =================================\033[0m"
+echo -e "\033[36mSetting sassdoc to run on startup\033[0m"
+echo -e "\033[36m=================================\033[0m"
 chkconfig --level 2345 sassdoc on
 
 service sassdoc start
-echo -e "\033[36m Done! Sassdoc is running at 192.168.192.10:3000\033[0m"
+echo -e "\033[36mDone! Sassdoc is running at 192.168.192.10:3000\033[0m"
