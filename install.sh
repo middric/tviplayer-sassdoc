e103411c4727eb5a502538a85534d3c31dad46a7
middric <richmiddleditch@gmail.com>
2013-05-26 10:22:26 +0100

    adding sassdoc submodule, install script, and service script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "Setting up TV & iPlayer Sassdoc\n\n"
+yum install git nodejs python26 ruby rubygems -y
+gem install compass -y
+npm -g install forever
+
+echo -e "Cloning tviplayer-sassdoc\n\n"
+git clone 
+cd tviplayer-sassdoc
+
+echo -e "Seting up sassdoc as a service\n\n"
+mv sassdoc-service /etc/init.d/sassdoc
+chmod a+x /etc/init.d/sassdoc
+
+echo -e "Setting sassdoc to run on startup\n\n"
+chkconfig --level 2345 sassdoc on
+
+cd sassdoc
+npm install --python=python26
+service sassdoc start
+echo -e "\n\nDone! Sassdoc is running at 192.168.192.10:3000"
